e8549c789a5d67fb4e74a6ae22c06aa10bdcfe36
navid <navidhosseini798@gmail.com>
2021-07-20 01:40:40 +0430

    sd

--- android/app/src/main/java/com/amlak/MainActivity.java
+++ android/app/src/main/java/com/amlak/MainActivity.java
@@ -2,6 +2,9 @@ package com.amlak;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen; 
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -18,4 +21,14 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "Amlak";
   }
+
+  @Override
+   protected ReactActivityDelegate createReactActivityDelegate() {
+   return new ReactActivityDelegate(this, getMainComponentName()) {
+    @Override
+     protected ReactRootView createRootView() {
+      return new RNGestureHandlerEnabledRootView(MainActivity.this);
+     }
+  };
+   }
 }
